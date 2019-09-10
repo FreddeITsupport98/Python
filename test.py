36733e7337e47ed0369a572c0097f868cbf431af
FreddeITsupport98 <freddebmcjobbmail@gmail.com>
2019-09-10 10:55:14 +0200

    love

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 
 print("Välkommen till mitt program där du kan addera")
-try:
-    tal1 = int(input("Mata in ett heltal: "))
-except:
-    print("Du måste skriva in en siffra")
+
+operator = input("Välj räknesätt (+ - * /): ")
+
+try: # Try testar cod blocken
+    tal1 = int(input("Mata in ett heltal: ")) #det testar tal1
+except: # except kör coden om fel blir"
+    print("Du måste skriva in en siffra") #print skriver ut resultat eller inskrivna input som använder har gjort
     tal1 = 0
 try:
     tal2 = int(input("Mata in ett till heltal: "))
@@ -11,5 +14,30 @@ except:
     print("Du måste skriva in en siffra")
     tal2 = 0
 
-summa = tal1 + tal2
-print("Summan är:", summa)
+if operator == "+": # if operator är lika med "+"
+    
+    summa = tal1 + tal2 
+
+elif operator == "-": # elif operator är lika med "-"
+
+
+    summa = (tal1 - tal2) 
+
+elif operator == "*":
+  
+    summa = (tal1 * tal2) 
+
+elif operator == "/":
+
+    try: 
+        summa = (tal1 / tal2) 
+        
+    except ZeroDivisionError: #Denna definarear noll divition som ett fel
+        print("Division by 0 is not acceptable!")
+   
+else: 
+    print("fel")
+
+    summa = int(tal1) + int(tal2)
+
+    print("Summan är:", summa)
